2cc41a99ca4e94b02db0a3adbe2e2972d4ea00a6
yuqi <yuqi@test.com>
2022-07-15 18:18:49 +0800

    暂时去掉注释

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App'
 import './index.css'
 // <React.StrictMode> 会导致两次渲染的检查,去掉则会正常
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 )
