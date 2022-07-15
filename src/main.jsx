34e0d37a86816e679a5a506c3e77d3b836f7d8d8
yuqi <yuqi@test.com>
2022-07-15 17:15:05 +0800

    一点确定的注释

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-
+// <React.StrictMode> 会导致两次渲染的检查,去掉则会正常
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
